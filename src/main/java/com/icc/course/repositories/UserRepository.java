87faacc96ac33c60ba84bbf8154cb08473d66afc
Iagoccarvalho <iago@hctech.com.br>
2022-03-16 16:35:06 -0300

    Service layer, component registration

--- src/main/java/com/icc/course/repositories/UserRepository.java
+++ src/main/java/com/icc/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.icc.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.icc.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
